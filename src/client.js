c4850afec818ea32252b40c4c4c2d22bba7b720a
nospyrin <mohammedzuhair527@gmail.com>
2020-04-10 04:54:36 +0200

    added request function for sending one time requests

--- src/client.js
+++ src/client.js
@@ -1,17 +1,18 @@
-import { EssosSocket } from './essos-protocol';
-import { handle } from './handler';
+import { EssosSocket, Request } from './essos-protocol';
+// import { handle } from './handler';
 import net from 'net';
 
 const { host, port } = require('../remote.json');
 
+process.env.HOST = host;
+process.env.PORT = port;
+
 export function init(cb) {
-    process.env.HOST = host;
-    process.env.PORT = port;
 
     // let socket;
     // try {
-        let socket = new net.Socket();
-        socket.connect(process.env.PORT, process.env.HOST);
+    let socket = new net.Socket();
+    socket.connect(process.env.PORT, process.env.HOST);
     // } catch (err) {
     //     console.log('Could not connect to the server!');
     //     console.log(err);
@@ -23,9 +24,29 @@ export function init(cb) {
         console.log(`Connected to ${process.env.HOST}:${process.env.PORT}`);
     });
 
-    let essSocket = new EssosSocket(socket);
+    // let essSocket = new EssosSocket(socket);
     console.log('Made it!');
 
     cb();
     // to-do: return Promise
+}
+
+export async function request(action, data) {
+    let socket = net.createConnection(process.env.PORT, process.env.HOST);
+    let essSocket = new EssosSocket(socket);
+
+    // send the req
+    essSocket.write(new Request(action, data));
+
+    // await the res
+    return await new Promise(resolve => {
+        socket.on('data', (chunk) => {
+            let res = essSocket.read(chunk);
+
+            if (res.length > 0) {
+                essSocket.clear();
+                resolve(res[0]);
+            }
+        });
+    });
 }
